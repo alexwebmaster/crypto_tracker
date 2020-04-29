e25b93266707a7aa70a71b9b4bd422f2daaa0f78
Alex  Gomes <contato@alexwebmaster.com.br>
2020-04-29 16:24:21 -0300

    added base features using  api

--- preload.js
+++ preload.js
@@ -1,12 +1,67 @@
+const { ipcRenderer, remote } = require('electron')
+const path = require('path')
+const axios = require('axios')
+
+let data;
+let notification = { title : 'Crypt Alert', body : 'Message', icon : path.join(__dirname, 'assets/icons/btc.png')}
+
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+window.addEventListener('DOMContentLoaded', () =>
+{
+  let low = document.getElementById("low");
+  let high = document.getElementById("max");
+  let last = document.getElementById("high");
+  let lower_alert = document.getElementById("lower_alert");
+  let higher_alert = document.getElementById("higher_alert");
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+  document.getElementById("close-btn").addEventListener("click", function (e) {
+     var window = remote.getCurrentWindow();
+     window.hide();
+  }); 
+	
+  checkApi();
+  //checking if is time to run
+  setInterval(checkApi, 160 * 1000);
 })
+
+const checkApi = () => {
+
+  console.log('checking api');
+
+  axios.get('https://api.bitcointrade.com.br/v3/public/BRLBTC/ticker').then(response => {
+    data = response.data.data; 
+    low.innerHTML = 'R$ '+data.low.toLocaleString('br');
+    last.innerHTML = 'R$ '+data.last.toLocaleString('br');
+    high.innerHTML = 'R$ '+data.high.toLocaleString('br');
+
+    checkCorners();
+  })
+}
+
+const checkCorners = () => {
+
+    let corner = document.getElementById("corner");
+
+    //check if it is low and going higher
+    let lower_mark = data.low+(data.low* (corner.value/100));
+    let higher_mark = data.high-(data.high* (corner.value/100));
+
+    lower_alert.innerHTML = 'R$ '+lower_mark.toLocaleString('br');
+    higher_alert.innerHTML = 'R$ '+higher_mark.toLocaleString('br');
+
+    // console.log('lower_mark: ' +lower_mark);
+    // console.log('higher_mark: ' +higher_mark);
+    if (data.last <= (lower_mark) ) {
+      showNotification("Crypto is near to lower corner")
+    }
+
+    if (data.last >= (higher_mark) ) {
+      showNotification('Crypto is near to higher corner');
+    }
+}
+
+const showNotification = (message) => {
+  notification.body = message;
+  const myNotification = new window.Notification(notification.title, notification);
+}
